5f4dcf73893223ae6a4222f188b1cf8ed48ef03b
Fabio Catino <fabivs9@gmail.com>
2021-11-25 10:59:22 +0100

    admin page

--- pages/admin.js
+++ pages/admin.js
@@ -1,17 +1,20 @@
 import { getProviders, signIn } from 'next-auth/react';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { GoogleLoginButton } from 'react-social-login-buttons';
 
 const Admin = ({ providers }) => {
 	return (
 		<div className='mt-48 self-center input-form'>
-			{console.log({providers})}
-			<GoogleLoginButton
-				GoogleLoginButton
-				onClick={() => signIn(providers.google.id, { callbackUrl: '/' })}
-			>
-				Sign in with {providers.google.name}
-			</GoogleLoginButton>
+			{Object.values(providers).map((provider) => (
+				<div key={provider.name}>
+					<GoogleLoginButton
+						GoogleLoginButton
+						onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+					>
+						Sign in with {provider.name}
+					</GoogleLoginButton>
+				</div>
+			))}
 		</div>
 	);
 };
@@ -20,7 +23,6 @@ export default Admin;
 
 export async function getServerSideProps() {
 	const providers = await getProviders();
-	console.log(providers);
 	return {
 		props: { providers },
 	};
