731b706e6aab8d6410be2abe50ccce3e6dde81ee
Fabio Catino <fabivs9@gmail.com>
2021-11-25 10:07:19 +0100

    admin page

--- pages/admin.js
+++ pages/admin.js
@@ -5,13 +5,12 @@ import { GoogleLoginButton } from 'react-social-login-buttons';
 const Admin = ({ providers }) => {
 	return (
 		<div className='mt-48 self-center input-form'>
-			<GoogleLoginButton
-				className=''
+			{providers ? <GoogleLoginButton
 				GoogleLoginButton
 				onClick={() => signIn(providers.google.id, { callbackUrl: '/' })}
 			>
 				Sign in with {providers.google.name}
-			</GoogleLoginButton>
+			</GoogleLoginButton> : Loading}
 		</div>
 	);
 };
